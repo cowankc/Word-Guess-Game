79edaf640bfbdabc877e1721c144101cd765e344
cowankc <cowankc92@live.com>
2019-08-09 22:14:25 -0500

    created a way to match the user and computer choice and record on the screen what was entered

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,33 +3,50 @@
 const playerList = ["ray allen", "tim duncan", "lebron james", "michael jordan", "larry bird", "magic johnson", "wilt chamberlain", "dikembe mutombo", "kobe bryant", "kareem abdul-jabbar", "charles barkley", "jerry west", "dirk nowitzki", "shaq", "dr.j", "karl malone", "steph curry", "yao ming", "david robinson", "bill russell"]
 
 let compGuess = playerList[Math.floor(Math.random() * playerList.length)];
-
-let counter = 10;
-
-let wins = 0;
-
-let blanks = [];
-
+let wordlength = compGuess.length;
+let correctLetters = [wordlength];
 let wordLetter = compGuess.split("");
+let counter = 10;
+let winCount = 0;
+let win = wordlength;
+let blanks = [];
+let userChoice = "";
+
+
 
 let createBlanks = function() {
     for(let i = 0; i < compGuess.length; i++) {
-        blanks.push("_");
+        correctLetters[i] = "_ "
+        blanks = blanks + correctLetters[i];
     }
-    return blanks
+    document.getElementById("chosenName").innerHTML = blanks;
 }
 
-document.getElementById("chosenName").innerHTML = createBlanks()
-
-let userChoice = document.onkeyup = function() {
+document.onkeyup = function () {
     let userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
     let storedLetter = document.createElement("h4");
     storedLetter.innerHTML = userGuess;
-    document.getElementById("usedletter").appendChild(storedLetter);
+    document.getElementById("usedletter").appendChild(storedLetter)
+    blanks = "";
+    for (let i=0; i< correctLetters.length; i++) {
+        if (userGuess === wordLetter[i]) {
+            correctLetters[i] = userGuess;
+            win--;
+        }
+        blanks = blanks + correctLetters[i] + " ";
+    }
+    document.getElementById("chosenName").innerHTML = blanks;
+    blanks = "";
 }
 
-if (userChoice = wordLetter) {
-    console.log("yes")
-}
+// document.onkeyup = function() {
+//     let userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+//     let storedLetter = document.createElement("h4");
+//     storedLetter.innerHTML = userGuess;
+//     document.getElementById("usedletter").appendChild(storedLetter)
+//     userGuess = userChoice;
+// }
+document.getElementById("guesses").innerHTML = counter;
+window.onload = createBlanks ()
 
 
